4a911e83354b6369065151ccad87b016fe1984d8
kashan-ahmad <git@oikashan.com>
2023-11-13 23:59:31 +0500

    feat: added hash based dropdowns

--- src/themes/useThemeDropdown.ts
+++ src/themes/useThemeDropdown.ts
@@ -1,14 +1,29 @@
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import { usePopupEffect } from "~/utils/hooks/usePopupEvents";
 
 export function useThemeDropdown<Dropdown extends HTMLElement>() {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const location = useLocation();
   const { ref: dropdownRef } = usePopupEffect<Dropdown>(() =>
     setIsDropdownOpen(false)
   );
 
-  function toggleDropdown() {
-    setIsDropdownOpen(!isDropdownOpen);
+  // Initial value is based on the hash itself.
+  // This ensures that the dropdown is truly controlled by the URL.
+  const isDropdownOpen = location.hash === "#themeDropdown";
+
+  /**
+   * Update the URL when the dropdown is opened or closed.
+   * This ensures that the dropdown is truly controlled by the URL.
+   *
+   * @param value Whether the dropdown is open or not.
+   */
+  function setIsDropdownOpen(value: boolean) {
+    if (value) {
+      location.hash = "#themeDropdown";
+    } else {
+      location.hash = "";
+    }
   }
 
   return {
