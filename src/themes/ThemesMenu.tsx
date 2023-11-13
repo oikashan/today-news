6012c531dcc084f939994f9aec42df6839bac2ec
kashan-ahmad <git@oikashan.com>
2023-11-13 23:54:54 +0500

    feat: added list of available themes

--- src/themes/ThemesMenu.tsx
+++ src/themes/ThemesMenu.tsx
@@ -1,3 +1,22 @@
+import { themes } from "./themes";
+import useThemes from "./useThemes";
+
 export function ThemesMenu() {
-  return <></>;
+  const { switchTheme } = useThemes();
+
+  return (
+    <ul>
+      {themes.map((theme, i) => (
+        <li key={i}>
+          <button
+            className="button-transparent"
+            title={`Switch to ${theme} theme`}
+            onClick={() => switchTheme(theme)}
+          >
+            <span className="text-menu-md">{theme}</span>
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
